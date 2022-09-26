d2d3c66dfb1ce26effe472a9e9b8f5242b658177
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-09-26 11:55:25 +0300

    Пофіксив лоадер
    Центрував + змінив колір

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,22 @@
 import { ThreeDots } from 'react-loader-spinner';
+import css from './Loader.module.css'
+
+const { loader } = css;
 
 function Loader() {
     return (
-        <ThreeDots
-            height="80"
-            width="80"
-            radius="9"
-            color="#4fa94d"
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{}}
-            wrapperClassName=""
-            visible={true}
-        />
+        <div className={loader}>
+            <ThreeDots
+                height="80"
+                width="80"
+                radius="9"
+                color="#3f51b5"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClassName=""
+                visible={true}
+            />
+        </div>
     );
 };
 
