d0cb55850e5832509a17799e8b40ffcf388668b0
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-09-26 08:15:10 +0300

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Children, Component } from "react";
+import { Component } from "react";
 import { createPortal } from "react-dom";
 import css from '../Modal/Modal.module.css'
 
@@ -8,7 +8,7 @@ const { overlay, modal } = css;
 class Modal extends Component {
   render() {
     // console.log(this.props.items)
-    const { largeImageURL, tags } = this.props.items;
+    // const { largeImageURL, tags } = this.props.items;
     const { children } = this.props;
     return createPortal(
       <div className={overlay}>
